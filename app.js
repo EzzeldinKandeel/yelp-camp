0be1ad1810e73a12cb0d4bcc26e28b24c6ef4ebb
Ezzeldin Kandeel <ezzeldinkandeel@gmail.com>
2024-06-14 21:31:38 +0300

    Started styling using Bootstrap

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const path = require("path");
 const Campground = require("./models/campground");
+const ejsMate = require("ejs-mate");
 
 mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
 const db = mongoose.connection;
@@ -14,7 +15,9 @@ db.once("open", () => {
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
